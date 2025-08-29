6bae0b0912d8144f0dd0ded1c41e08449854ca3e
Felipe <felipecattosilvabr@gmail.com>
2025-08-29 11:59:27 -0300

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,12 @@ import Logo from "../assets/images.jpg"
 
 const Nav = () => {
     return (
-        <header className="bg-gray-800 text-white p-4 shadow-md">
+        <header className="bg-gray-900 text-white p-4 shadow-md">
             <nav className="container mx-auto flex justify-between items-center">
                 <img src={Logo} alt="logo" className="h-10 w-auto"/>
                 <ul className="flex space-x-4">
-                    <li className="list-none"><Link to="/" className="hover:text-gray-500 transition-all duration-300">Home</Link></li>
-                    <li><Link to="/cliente">Cliente</Link></li>
+                    <li className="list-none"><Link to="/" className="hover:text-gray-500 transition-all duration-150">Home</Link></li>
+                    <li><Link to="/cliente" className="hover:text-gray-500 transition-all duration-150">Cliente</Link></li>
                 </ul>
             </nav>
         </header>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import imagem from '../assets/images.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="bg-gray-800 text-white min-h-screen flex flex-col md:flex-row items-center justify-center p-8">
+      <div className="flex-1 flex-col items-center md:items-start text-center md:text-left mb-8 md:mb-0 mr-16">
+        <p className="text-1g">Loja de Carros</p>
+        <h4 className="text-4x1 sm:text-5xl md:text-6xl font-bold mt-2">O melhor Carro<br/>de Corrida</h4>
+      </div>
+      <button className="mt-8 px-6 py-3 text-white font-extrabold bg-blue-900">Participar</button>
+      <div className='flex-1 items-center justify-center'><img src={imagem} alt='carrro' className='w-full max-w-sm md:max-w-md'></img></div>
+    </section>
   )
 }
 
