bb079f90ede76da2dedfb1aaae7246a960c64134
Felipe <felipecattosilvabr@gmail.com>
2025-08-29 11:45:47 -0300

    adicionando rotas e estilizando nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+import Error from "./routes/Error"
+import Home from "./routes/Home"
+import Cliente from "./routes/Cliente"
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Nav/>
+    <main>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cliente" element={<Cliente/>}/>
+
+      </Routes>
+    </main>
+    <Footer/>
+    </BrowserRouter>
   )
 }
 
